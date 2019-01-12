7e8f12e3e3e2dbba78585d1ce45254b614351a7a
RitikaAg <ritikaagarwal646@gmail.com>
2019-01-12 16:00:14 +0530

    added comments

--- tags.py
+++ tags.py
@@ -1,12 +1,12 @@
 import pydicom
-bmode_ds = pydicom.read_file("bmode.dcm")
-ttfm_ds = pydicom.read_file('ttfm.dcm')
+bmode_ds = pydicom.read_file("bmode.dcm")#reading the file
+ttfm_ds = pydicom.read_file('ttfm.dcm')#reading the file
 
 with open('bmode_dtags.txt', 'w+') as f:
     for item in bmode_ds.keys():
-        f.write("%s\n" % item)
+        f.write("%s\n" % item)#writing tags to file
 f.close()
 with open('ttfm_dtags.txt', 'w+') as f1:
     for item in ttfm_ds.keys():
-        f1.write("%s\n" % item)
+        f1.write("%s\n" % item)#writing tags to file
 f1.close()
